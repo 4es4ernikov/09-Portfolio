ca9331bb73c6f59db7e5b503f2c3f4b292169a87
4es4ernikov <80171567+4es4ernikov@users.noreply.github.com>
2022-04-27 23:07:39 +0300

    script

---
+++ recourses/scriptMain.js
@@ -0,0 +1,20 @@
+let element = document.getElementById('interact1');
+let makeTheBorder = () => {
+element.style.border = '1px solid red ';
+}
+
+element.onmousedown = makeTheBorder;
+
+let element2 = document.getElementById('interact2');
+let makeTheBorder2 = () => {
+element2.style.border = '1px solid red ';
+}
+
+element2.onmousedown = makeTheBorder2;
+
+let element3 = document.getElementById('interact3');
+let makeTheBorder3 = () => {
+element3.style.border = '1px solid red ';
+}
+
+element3.onmousedown = makeTheBorder3;
